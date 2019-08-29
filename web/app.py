6a5acfdc75251ac749ae101cf16304f8c6bd37dd
chandannaidu <chandan.naidu97@gmail.com>
2019-08-30 05:04:52 +0530

    final commit

--- web/app.py
+++ web/app.py
@@ -12,19 +12,31 @@ client = MongoClient("mongodb://db:27017")
 db = client.SimlarityDB
 users = db["Users"]
 
-def UserExist(username):
-    if users.find({"username":username}).count() == 0 :
+def UserExist(username): 
+    if users.find({"Username":username}).count() == 0:
         return False
     else:
         return True
 
 def verifyPW(username, password):
-    pass
+    if not UserExist(username):
+        return False
+    hashed_pw = users.find({
+        "Username" : username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw)==hashed_pw:
+        return True
+    else:
+        return False
 
 def count_Tokens(username):
-    pass
+    tokens = users.find({
+        "Username" : username
+    })[0]["Tokens"]
+    return tokens
 
-class Regiester (Resource):
+class Register(Resource):
     def post(self):
         postedData = request.get_json()
 
@@ -33,7 +45,7 @@ class Regiester (Resource):
 
         if UserExist(username):
             retJson = {
-                "status" : 301,
+                "status" : "301",
                 "msg" : "Invalid Username"
             }
             return jsonify(retJson)
@@ -51,9 +63,9 @@ class Regiester (Resource):
                 "status" : 200,
                 "msg" : "you have successfully signed up to an API "
             }
-        jsonify(retJson)
+        return jsonify(retJson)
     
-class detect(Resource):
+class Detect(Resource):
     def post(self):
         postedData = request.get_json()
 
@@ -64,7 +76,7 @@ class detect(Resource):
 
         if UserExist(username):
             retJson = {
-                "status" : 301,
+                "status" : "301",
                 "msg" : "Invalid Username"
             }
             return jsonify(retJson)
@@ -73,19 +85,23 @@ class detect(Resource):
 
         if not correct_pw:
             retJson = {
-            "status" : 301,
+            "status" : 302,
             "msg" : "Invalid Password"
-        }
+            }
+            return jsonify(retJson)
 
         num_token = count_Tokens(username)
 
         if num_token <= 0:
             retJson = {
             "status" : 303,
-            "msg" : "Out of tockens"
-        }
+            "msg" : "Out of tokens"
+            }
+            return jsonify(retJson)
 
-        nlp = spacy.load('en_core_web_sm-2.0.0.tar.gz')
+
+        # nlp = spacy.load('en_core_web_sm')
+        nlp = spacy.load('en_core_web_sm')
 
         text1 = nlp(text1)
         text2 = nlp(text2)
@@ -96,19 +112,72 @@ class detect(Resource):
             "status" : 200,
             "similarity" : ratio,
             "msg" : "similarity tested successfully"
-        }
+            }
+
 
         current_tokens = count_Tokens(username)
 
         users.update({
-            "Username" : username
-        },{
+            "Username" : username,
+        },
+        {
+            "$set":{
             "Tokens" : current_tokens - 1
+            }
+        
         })
 
         return jsonify(retJson)
 
+class Refill(Resource):
+    def Post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["admin_pw"]
+        refill_amount = postedData["refill"]
+
+        if not UserExist(username):
+            retJson = {
+                "status" : 301,
+                "msg" : "Invalid Username"
+            }
+            return jsonify(retJson)
+
+        correct_pw = "abc123"
+
+        if not password == correct_pw:
+            retJson = {
+                "status" : 304,
+                "msg" : "Invalid admin Password"
+            }
+            return jsonify(retJson)
         
+        current_tokens = count_Tokens(username)
+        users.update({
+            "Username" : username
+        },
+        {
+            "$set":{
+                "Tokens": refill_amount+current_tokens
+            }
+        })
+
+        retJson = {
+                "status" : 200,
+                "msg" : "Successfully creited"
+            }
+        return jsonify(retJson)
+
+api.add_resource(Register, '/register')
+api.add_resource(Detect, '/detect')
+api.add_resource(Refill, '/refill')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+        
+
 
 
 
